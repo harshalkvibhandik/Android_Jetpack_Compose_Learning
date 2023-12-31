f85973febd096467d4f863360ed0b3c21b4004a6
Harshal Vibhandik <harshak.k.vibhandik@gmail.com>
2023-12-31 12:37:51 +0530

    Introduction to Column and Image composable components

--- app/src/main/java/com/harshalv/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/harshalv/jetpackcompose/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.harshalv.jetpackcompose.ui.theme.DogProfilePageTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,30 +19,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Todo 3: Call the MYApp function for Application first Screen
-                    MyApp()
+                    // Todo 6: Add the ProfilePage as the device display
+                    ProfilePage()
                 }
             }
         }
-    }
-}
-
-//Todo 1 create a new compose function and call the Greeting function
-@Composable
-fun MyApp() {
-    Greeting("Welcome To Jetpack Compose Learning")
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello, $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DogProfilePageTheme {
-        //Todo 2: call the MyApp function for preview
-        MyApp()
     }
 }
