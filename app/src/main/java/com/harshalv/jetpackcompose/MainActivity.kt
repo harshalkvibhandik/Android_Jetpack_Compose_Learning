0599602208ac07d51437374c80d9ff182f5833e4
Harshal Vibhandik <harshak.k.vibhandik@gmail.com>
2023-12-31 15:02:15 +0530

    Made GmailApp Clone using Scaffold and Dialogs

--- app/src/main/java/com/harshalv/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/harshalv/jetpackcompose/MainActivity.kt
@@ -3,26 +3,31 @@ package com.harshalv.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
-import com.harshalv.jetpackcompose.ui.theme.JetpackComposeTheme
+import com.harshalv.jetpackcompose.components.GmailDrawerMenu
+import com.harshalv.jetpackcompose.components.GmailFab
+import com.harshalv.jetpackcompose.components.HomeAppBar
+import com.harshalv.jetpackcompose.components.HomeBottomMenu
+import com.harshalv.jetpackcompose.components.MailList
+import com.harshalv.jetpackcompose.ui.theme.ComposeMaterialDesignSamplesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
+            ComposeMaterialDesignSamplesTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    GmailApp()
                 }
             }
         }
@@ -30,17 +35,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GmailApp() {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
+    val openDialog = remember {
+        mutableStateOf(false)
+    }
+
+    Scaffold(scaffoldState = scaffoldState,
+        topBar = {
+            HomeAppBar(scaffoldState, coroutineScope, openDialog)
+        },
+        drawerContent = {
+            GmailDrawerMenu(scrollState = scrollState)
+        },
+        bottomBar = {
+            HomeBottomMenu()
+        },
+        floatingActionButton = {
+            GmailFab(scrollState)
+        }) {
+        MailList(it, scrollState)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+fun DefaultPreview() {
+    ComposeMaterialDesignSamplesTheme {
+        GmailApp()
     }
 }
---
+++ app/src/main/java/com/harshalv/jetpackcompose/components/HomeBottomMenu.kt
@@ -0,0 +1,38 @@
+package com.harshalv.jetpackcompose.components
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.harshalv.jetpackcompose.R
+import com.harshalv.jetpackcompose.model.BottomMenuData
+
+@Composable
+fun HomeBottomMenu() {
+    val items = listOf(
+        BottomMenuData.Mail,
+        BottomMenuData.Meet
+    )
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.white),
+        contentColor = colorResource(id = R.color.black)
+    )
+    {
+        items.forEach {
+            BottomNavigationItem(
+                label = { Text(text = it.title) },
+                alwaysShowLabel = true,
+                selected = false,
+                onClick = { },
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+            )
+        }
+    }
+}
